4cc18b86c34c5a8eb79f2f0c905f9bbc632b908d
Pim Vermeer <pim.vermeer@gmail.com>
2025-04-05 12:05:14 +0000

    feat(main-menu): added main menu + about

--- src/application/window.rs
+++ src/application/window.rs
@@ -1,22 +1,46 @@
 use libadwaita::{
-    HeaderBar, NavigationPage, NavigationSplitView, ToolbarView,
+    AboutWindow, HeaderBar, NavigationPage, NavigationSplitView, ToolbarView,
+    gio::{ActionEntry, Menu, MenuItem, SimpleActionGroup, prelude::ActionMapExtManual},
     glib::object::IsA,
-    gtk::{self, ListBox, Orientation, SelectionMode, prelude::GtkWindowExt},
+    gtk::{
+        self, ListBox, MenuButton, Orientation, SelectionMode,
+        prelude::{GtkWindowExt, WidgetExt},
+    },
 };
+
+use crate::config;
+
+pub struct MainMenu {
+    menu: Menu,
+    button: MenuButton,
+    actions: SimpleActionGroup,
+}
 
 pub struct ApplicationWindow {
     pub window: libadwaita::ApplicationWindow,
+    pub main_menu: MainMenu,
     pub sidebar_list: ListBox,
     pub content_box: gtk::Box,
 }
 impl ApplicationWindow {
+    const MAIN_MENU_ACTION_LABEL: &str = "main-menu";
+
     pub fn new(adw_application: &libadwaita::Application, title: &str) -> Self {
         let title = title.to_string();
         let list = Self::build_list();
         let content_box = gtk::Box::new(Orientation::Vertical, 0);
 
-        let sidebar = Self::build_view(&title, &list);
-        let content = Self::build_view("Content", &content_box);
+        let main_menu_actions = SimpleActionGroup::new();
+        let (main_menu_button, main_menu) = Self::build_menu(&main_menu_actions);
+        let main_menu = MainMenu {
+            menu: main_menu,
+            button: main_menu_button,
+            actions: main_menu_actions,
+        };
+
+        let (sidebar, sidebar_header) = Self::build_view(&title, &list);
+        let (content, _content_header) = Self::build_view("Content", &content_box);
+        sidebar_header.pack_end(&main_menu.button);
 
         let split_view = NavigationSplitView::builder()
             .sidebar(&sidebar)
@@ -30,9 +54,11 @@ impl ApplicationWindow {
             .default_width(800)
             .content(&split_view)
             .build();
+        window.insert_action_group(Self::MAIN_MENU_ACTION_LABEL, Some(&main_menu.actions));
 
         let application_window = Self {
             window,
+            main_menu,
             sidebar_list: list,
             content_box,
         };
@@ -44,7 +70,18 @@ impl ApplicationWindow {
         self.window.present();
     }
 
-    fn build_view(title: &str, content: &impl IsA<gtk::Widget>) -> NavigationPage {
+    fn show_about() {
+        let about = AboutWindow::new();
+        about.set_application_name(config::APP_NAME);
+        about.set_version(config::VERSION);
+        about.set_developer_name("Me");
+        about.add_credit_section(Some("Code by"), &["Me"]);
+        about.add_acknowledgement_section(None, &["Also me"]);
+        about.add_legal_section("Some title", None, gtk::License::Gpl30, None);
+        about.show();
+    }
+
+    fn build_view(title: &str, content: &impl IsA<gtk::Widget>) -> (NavigationPage, HeaderBar) {
         let header = HeaderBar::new();
         let toolbar = ToolbarView::new();
         toolbar.add_top_bar(&header);
@@ -55,7 +92,7 @@ impl ApplicationWindow {
             .child(&toolbar)
             .build();
 
-        return page;
+        return (page, header);
     }
 
     fn build_list() -> ListBox {
@@ -68,5 +105,43 @@ impl ApplicationWindow {
             // makes the list look nicer
             .css_classes(vec![String::from("boxed-list")])
             .build();
+    }
+
+    fn build_menu(action_group: &SimpleActionGroup) -> (MenuButton, Menu) {
+        // GTK does not let a popovermenu to be created programmatically
+        // https://blog.libove.org/posts/rust-gtk--creating-a-menu-bar-programmatically-with-gtk-rs
+        let menu_button = MenuButton::builder()
+            .name("main-menu")
+            .icon_name("open-menu-symbolic")
+            .build();
+        let main_menu = Menu::new();
+
+        main_menu.prepend_item(&Self::build_menu_item(
+            "About",
+            ("about", || Self::show_about()),
+            action_group,
+        ));
+
+        menu_button.set_menu_model(Some(&main_menu));
+        return (menu_button, main_menu);
+    }
+
+    fn build_menu_item(
+        label: &str,
+        (action_name, action): (&str, impl Fn() + 'static),
+        action_group: &SimpleActionGroup,
+    ) -> MenuItem {
+        let menu_item = MenuItem::new(
+            Some(label),
+            Some(&(Self::MAIN_MENU_ACTION_LABEL.to_owned() + "." + action_name)),
+        );
+        let menu_item_action = ActionEntry::builder(action_name)
+            .activate(move |_: &SimpleActionGroup, _, _| {
+                action();
+            })
+            .build();
+        action_group.add_action_entries([menu_item_action]);
+
+        return menu_item;
     }
 }
