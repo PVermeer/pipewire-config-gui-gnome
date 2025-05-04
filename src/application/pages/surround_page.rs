b5650d69462d667c9437f2a6c79296c624abad2e
Pim Vermeer <pim.vermeer@gmail.com>
2025-05-04 11:28:30 +0000

    feat(surround): [wip] started default implementation

--- src/application/pages/surround_page.rs
+++ src/application/pages/surround_page.rs
@@ -4,11 +4,12 @@ use crate::application::{
     pipewire::pipewire::Pipewire,
     shared::init::{Init, InitTrait},
 };
+use convert_case::{Case, Casing};
 use libadwaita::{
-    NavigationPage,
-    gtk::{self, Label, prelude::BoxExt},
+    NavigationPage, SpinRow, SwitchRow,
+    glib::object::Cast,
+    gtk::{self, Adjustment, Label, Widget, prelude::BoxExt},
 };
-use serde_json::Value;
 use std::{collections::HashMap, rc::Rc};
 
 pub struct SurroundPage {
@@ -46,16 +47,17 @@ impl SurroundPage {
     fn on_onit(&self, application: Rc<Application>) {
         let pipewire = application.pipewire.clone();
 
-        let labels = self.build_default_sections(pipewire);
+        self.build_default_sections(pipewire);
+        // let labels = self.build_default_sections(pipewire);
 
-        for label in labels {
-            self.content_box.append(&label);
-        }
+        // for label in labels {
+        //     self.content_box.append(&label);
+        // }
     }
 
-    fn build_default_sections(&self, pipewire: Rc<Pipewire>) -> Vec<Label> {
+    fn build_default_sections(&self, pipewire: Rc<Pipewire>) {
         let default = &pipewire.surround.default;
-        let mut map: HashMap<Option<&str>, Vec<(&str, &Value)>> = HashMap::new();
+        let mut map: HashMap<Option<&str>, Vec<(&str, &serde_json::Value)>> = HashMap::new();
 
         for (key, value) in default {
             let mut section = None;
@@ -77,12 +79,12 @@ impl SurroundPage {
             mapped_section.push((prop, value));
         }
 
-        let mut labels: Vec<Label> = Vec::new();
+        // let mut labels: Vec<Label> = Vec::new();
 
         for (section_name, values) in map {
             let label_markup = match section_name {
-                None => format!("<b></b>{:?}", values),
-                Some(value) => format!("<b>{}</b>{:?}", value, values),
+                None => format!("<b>Misc</b>"),
+                Some(section) => format!("<b>{}</b>", section.to_case(Case::Title)),
             };
 
             let label = Label::builder()
@@ -92,8 +94,47 @@ impl SurroundPage {
                 .halign(gtk::Align::Start)
                 .build();
 
-            labels.push(label);
+            self.content_box.append(&label);
+            // labels.push(label);
+            for (key, value) in values {
+                self.content_box.append(&self.build_input_row(key, value));
+            }
         }
-        labels
+        // labels
+    }
+
+    fn build_input_row(&self, key: &str, value: &serde_json::Value) -> Widget {
+        let title = key.from_case(Case::Kebab).to_case(Case::Sentence);
+
+        match value {
+            serde_json::Value::Bool(value) => SwitchRow::builder()
+                .title(title)
+                .active(value.to_owned())
+                .build()
+                .upcast(),
+
+            // TODO: maybe slider? (ActionRow->Scale)
+            serde_json::Value::Number(value) => SpinRow::builder()
+                .title(title)
+                .adjustment(
+                    &Adjustment::builder()
+                        .lower(f64::MIN)
+                        .upper(f64::MAX)
+                        .page_increment(1.0)
+                        .step_increment(1.0)
+                        .build(),
+                )
+                .digits(1)
+                .editable(true)
+                .value(value.as_f64().unwrap())
+                .build()
+                .upcast(),
+
+            // serde_json::Value::String(_value) => Row::None,
+            // serde_json::Value::Array(_value) => Row::None,
+            // serde_json::Value::Object(_value) => Row::None,
+            // serde_json::Value::Null => Row::None,
+            _ => SwitchRow::builder().title(title).build().upcast(),
+        }
     }
 }
